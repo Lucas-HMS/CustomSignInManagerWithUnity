8ea83e39b2a896b72ef281aa215a8595561bc90c
Luca Egli <luca.egli@hmsag.ch>
2021-02-23 09:30:29 +0100

    Add project files.

---
+++ CustomSignInManagerWithUnity/SomeCustomSignInManager.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomSignInManagerWithUnity
+{
+    public class SomeCustomSignInManager : SignInManager<IdentityUser>
+    {
+        public SomeCustomSignInManager(UserManager<IdentityUser> userManager, IHttpContextAccessor contextAccessor, IUserClaimsPrincipalFactory<IdentityUser> claimsFactory, IOptions<IdentityOptions> optionsAccessor, ILogger<SignInManager<IdentityUser>> logger, IAuthenticationSchemeProvider schemes, IUserConfirmation<IdentityUser> confirmation) : base(userManager, contextAccessor, claimsFactory, optionsAccessor, logger, schemes, confirmation)
+        {
+        }
+
+        // Here be some overridden methods (Not necessary for bug reproduction)
+    }
+}
